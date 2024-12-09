1069fbccb58720cec3f29199e51bd4afda9f60ba
BoneDeath <mhasannudin300@gmail.com>
2024-12-09 11:50:13 +0700

    add key

--- src/components/ListMahasiswa.vue
+++ src/components/ListMahasiswa.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import axios from 'axios';
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import type { Mahasiswa } from '@/type/mahasiswa';
 const mahasiswa = ref<Mahasiswa[]>([]);
 
@@ -8,20 +8,26 @@ async function getMahasiswa() {
     try {
         const response = await axios.get('http://localhost:3434/mahasiswa')
         mahasiswa.value = response.data;
+        console.log(mahasiswa)
     } catch (error) {
         console.log(error)
     }
 }
+onMounted(async()=>{
+    await getMahasiswa()
 
-getMahasiswa()
+})
+
 </script>
 
 <template>
     <div>
-        <li v-for="dataMahasiswa in mahasiswa">
+        <li v-for="dataMahasiswa in mahasiswa" :key="dataMahasiswa._id.toString">
+            ID: {{ dataMahasiswa._id }}<br>
             Nim: {{ dataMahasiswa.nim }}<br>
             Nama: {{ dataMahasiswa.nama }}<br>
             Prodi: {{ dataMahasiswa.prodi }}
+
         </li>
     </div>
 </template>
--- src/type/mahasiswa.ts
+++ src/type/mahasiswa.ts
@@ -1,4 +1,7 @@
+import type { ObjectId } from "mongodb";
+
 export interface Mahasiswa {
+    _id:ObjectId,
     nim:String,
     nama:String,
     prodi:String
